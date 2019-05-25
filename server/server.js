5448840618e39c8510c1a8ef036220f8b00639c9
Dommir Figueroa <dommirjose@gmail.com>
2019-05-25 18:16:08 -0300

    add cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 const app = express();
 
 let port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('./routes/index'));
