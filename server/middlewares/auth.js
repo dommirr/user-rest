dff9181fdf505dd00d94d6fe75d65d0b43929ade
David Figueroa <davidfigueroa@MacBook-Pro-de-Viviannys-Diaz.local>
2019-06-02 16:34:27 -0300

    finish integration with travis and added test and lint commands

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+
 const verifyToken = (req, res, next) => {
 
   const token = req.get('token');
 
-  jwt.verify(token, process.env.SEED, (err, decoded) => {
+  jwt.verify(token, process.env.SEED || 'teststring' , (err, decoded) => {
     if (err) {
       res.status(401).json({
         ok: false,
